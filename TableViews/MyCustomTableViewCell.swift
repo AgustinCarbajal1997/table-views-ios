b061cdcffc9ffd222b3ec4fdcfbddbb05045464f
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-10-28 20:28:54 -0300

    [feature] custom view cells

---
+++ TableViews/MyCustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyCustomTableViewCell.swift
+//  TableViews
+//
+//  Created by Agustin Carbajal on 28/10/2023.
+//
+
+import UIKit
+
+class MyCustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
