826f6475ef0e2b38e03a063c01227806b4c3cba4
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-10-28 17:59:05 -0300

    [feature] cell views

--- TableViews/ViewController.swift
+++ TableViews/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var TableView: UITableView!
+    
+    private let myCountries = ["Argentina", "Mexico", "España", "Estados Unidos"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        TableView.dataSource = self
+        TableView.tableFooterView = UIView() // para evitar que me cree celdas vacias para rellenar espacio
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myCountries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // siempre reutilizar celdas ya creadas, para eso le asigno un identificador
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            // si nunca se creo, se la crea de la siguiente forma
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+            cell?.backgroundColor = .green
+            cell?.textLabel?.font = UIFont.systemFont(ofSize: 20)
+        }
+        
+        cell!.textLabel?.text = myCountries[indexPath.row]
+        return cell!
+    }
+    
+    
 }
 
